75e59da5208dd88879a80487fe1e813a977b9cd9
wayne89232 <wayne89232@wayne89232de-MacBook-Pro.local>
2016-11-18 16:25:01 +0800

    fix state

--- routes/ranking.js
+++ routes/ranking.js
@@ -1,34 +1,35 @@
-// var _ = require('underscore');
-// var Mission = require('../models').Mission;
-// var User = require('../models').User;
+var _ = require('underscore');
+var Mission = require('../models').Mission;
+var User = require('../models').User;
 
 
 
-// //calculate by completed mission's rating
-// exports.tool_ranking = function(req, res){
-// 	Toolship.findAll({
-// 		incude: [{
-// 			model: Mission,
-// 			through: {
-// 				where: {status: "Done"}
-// 			}
-// 		}]
-// 	}).then(function(result){
-// 		event_list = _.map(result, function(result){
-// 			return result.dataValues;
-// 		});
-// 		res.json({ data: event_list });
-// 	});
-// }
+//calculate by completed mission's rating
+exports.tool_ranking = function(req, res){
+	Toolship.findAll({
+		incude: [{
+			model: Mission,
+			through: {
+				where: {status: "Done"}
+			}
+		}]
+	}).then(function(result){
+		//change order here, group user
+		event_list = _.map(result, function(result){
+			return result.dataValues;
+		});
+		res.json({ data: event_list });
+	});
+}
 
-// //By the number of mission hosted
-// exports.user_ranking = function(req, res){
-// 	Toolship.findAll({
-// 		incude
-// 	}).then(function(result){
-// 		event_list = _.map(result, function(result){
-// 			return result.dataValues;
-// 		});
-// 		res.json({ data: event_list });
-// 	});
-// }
+//By the number of mission hosted
+exports.user_ranking = function(req, res){
+	Toolship.findAll({
+		incude
+	}).then(function(result){
+		event_list = _.map(result, function(result){
+			return result.dataValues;
+		});
+		res.json({ data: event_list });
+	});
+}
