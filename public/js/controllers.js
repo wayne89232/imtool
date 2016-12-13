2f8b03aef2003bb4d7af54929768055033917bdb
李昱賢 <rick830620@gmail.com>
2016-12-13 15:12:01 +0800

    1. Chat Room View Done 2. Add Community List Page

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.controllers', ['ngRoute','ngFileUpload']).controller('AppCtrl', function ($rootScope, $window, $scope, $http, $location) {
+angular.module('myApp.controllers', ['ngRoute','ngFileUpload','luegg.directives']).controller('AppCtrl', function ($rootScope, $window, $scope, $http, $location) {
 	if($window.localStorage.getItem("is_login")){
 		$scope.is_login = true;
 		$scope.local_user = $window.localStorage.getItem("account");
