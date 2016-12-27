9b15cd97300a533989e13a39dcc7fa79326dd11f
r05725032 <r05725032@ntu.edu.tw>
2016-12-27 20:40:19 +0800

    說

--- routes/ranking.js
+++ routes/ranking.js
@@ -2,17 +2,19 @@ var _ = require('underscore');
 var Mission = require('../models').Mission;
 var User = require('../models').User;
 var Toolship = require('../models').Toolship;
+var User_skill = require('../models').User_skill;
 var async = require('async');
 
 //calculate by completed mission's rating
 exports.tool_ranking = function(req, res){
 	console.log("123")
+
 	Toolship.findAll({
 		//SELECT toolship.user_id, toolship.AVG(rating),  															
 		//SELECT COUNT(*) FROM MIssions WHERE  Missions.user_id=Toolship.user_id
 		//FROM toolship GROUP BY user_id
 
-		attributes:['user_id',[Toolship.sequelize.fn('AVG', Toolship.sequelize.col('rating')),'rank'],[Toolship.sequelize.fn('COUNT', Toolship.sequelize.col('user_id')), 'number']],
+		attributes:['user_id',[Toolship.sequelize.fn('AVG', Toolship.sequelize.col('rating')),'rank'],[Toolship.sequelize.fn('COUNT', Toolship.sequelize.col('user_id')), 'completedMission']],
 		
 		// where:{
 		// 	user_id: req.params.user_id
@@ -23,7 +25,14 @@ exports.tool_ranking = function(req, res){
 
 		order: [[Toolship.sequelize.fn('AVG', Toolship.sequelize.col('rating')), 'DESC']],
 
-		// include: [User]
+		where: {
+    		rating: {$ne: null}
+  		},
+
+		// include: [{
+		// 	association: Toolship.belongsTo(User),
+  //   		include:  User.hasMany(User_skill) 
+		// }]
 	}).then(function(result){
 		//change order here, group user
 
@@ -33,18 +42,19 @@ exports.tool_ranking = function(req, res){
 
 		    // Perform operation on file here.
 		    var resultData = element.dataValues;
-		    console.log(resultData);
+		    // console.log(resultData);
 
 		    User.findOne({
 		    	where: {
 		    		user_id: resultData.user_id
+
 		    	}
 		    }).then(function(response){
 		    	if (response){
 		    		var data = response.dataValues;
-		    		console.log("UserID: ", data);
+		    		// console.log("UserID: ", data);
 		    		resultData.userPhoto = data.photo_url
-		    		console.log(resultData)
+		    		// console.log(resultData)
 		    	}
 		    	resultDatas.push(resultData);
 		    	callback();
@@ -69,14 +79,66 @@ exports.tool_ranking = function(req, res){
 //By the number of mission hosted
 exports.function_ranking = function(req, res){
 	Toolship.findAll({
-		//SELECT toolship.user_id, toolship.AVG(rating) FROM toolship GROUP BY user_id WHERE 
-		incude: [{
-			model: Mission,
-		}]
+		//SELECT toolship.user_id, toolship.AVG(rating),  															
+		//SELECT COUNT(*) FROM MIssions WHERE  Missions.user_id=Toolship.user_id
+		//FROM toolship GROUP BY user_id
+
+		attributes:['user_id',[Toolship.sequelize.fn('AVG', Toolship.sequelize.col('rating')),'rank'],[Toolship.sequelize.fn('COUNT', Toolship.sequelize.col('user_id')), 'completedMission']],
+		
+		// where:{
+		// 	user_id: req.params.user_id
+		//},
+		// group: 'user_id',
+
+		group: ['user_id'],
+
+		order: [[Toolship.sequelize.fn('AVG', Toolship.sequelize.col('rating')), 'DESC']],
+
+		where: {
+    		rating: {$ne: null}
+  		},
+
+		// include: [User_skill]
 	}).then(function(result){
-		event_list = _.map(result, function(result){
-			return result.dataValues;
+		//change order here, group user
+
+		var resultDatas = [];
+
+		async.each(result, function(element, callback) {
+
+		    // Perform operation on file here.
+		    var resultData = element.dataValues;
+		    // console.log(resultData);
+
+		    User.findOne({
+		    	where: {
+		    		user_id: resultData.user_id
+		    	},
+				include:[User_skill]
+		    }).then(function(response){
+		    	if (response){
+		    		var data = response.dataValues;
+		    		// console.log("UserID: ", data);
+		    		resultData.userPhoto = data.photo_url
+		    		resultData.userSkill = data.User_skill
+		    		console.log(resultData)
+		    	}
+		    	resultDatas.push(resultData);
+		    	callback();
+		    }).catch(function(error){
+		    	callback(error);
+		    })
+		}, function(err) {
+			// if any of the file processing produced an error, err would equal that error
+			 if( err ) {
+				// One of the iterations produced an error.
+				// All processing will now stop.
+				console.log('A file failed to process');
+		    } else {
+		    	console.log('All files have been processed successfully');
+		    	console.log("Result: ",resultDatas);
+		    	res.json({ data: resultDatas });
+			}
 		});
-		res.json({ data: event_list });
 	});
 }
