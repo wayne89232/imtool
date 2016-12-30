9526bab54c2d05918ee0586f642dc7650dbb5e08
wayne89232 <wayne89232@gmail.com>
2016-12-30 15:12:59 +0800

    dump fix

--- setting/imtool_dump.sql
+++ setting/imtool_dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 30, 2016 at 08:02 AM
+-- Generation Time: Dec 30, 2016 at 08:12 AM
 -- Server version: 5.6.28
 -- PHP Version: 7.0.10
 
@@ -21,16 +21,57 @@ SET time_zone = "+00:00";
 --
 
 --
+-- Dumping data for table `mission`
+--
+
+INSERT INTO `mission` (`mission_id`, `user_id`, `location_id`, `title`, `content`, `recruit_time`, `start_time`, `expire_time`, `state`, `createdAt`, `updatedAt`) VALUES
+('1', '19dddecfa2176953057f16f63ba3d52f', NULL, 'Bike', 'Bike', NULL, NULL, NULL, 'Recruiting', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('2', 'e59bf7cd0b41e35b7ba50fc8e71cdf64', NULL, 'Test', 'test', NULL, NULL, NULL, 'Recruiting', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('3', 'cdf02830e6eba153d68595d64f5d397a', NULL, '徵女一舍到管院單程前往車夫一名 ><', '徵女一舍到管院單程前往車夫一名 ><', NULL, NULL, NULL, 'Recruiting', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+--
 -- Dumping data for table `mission_skill`
 --
 
 INSERT INTO `mission_skill` (`mission_skill_id`, `skill_id`, `mission_id`, `createdAt`, `updatedAt`) VALUES
 ('1', '1', '3', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-('2', '2', '3', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-('613530f050ad312f729db5fb40937dade6c33fee', '1', '870d2e25f28a3b1ccf34d3da9ce0daf93fe50445', '2016-12-29 08:30:20', '2016-12-29 08:30:20'),
-('dd29771055dcaf4b0bc4e8dbdf5a938867b2d51b', 'ab90f706026637e3e679263dd35c7571750d480b', '870d2e25f28a3b1ccf34d3da9ce0daf93fe50445', '2016-12-29 08:30:20', '2016-12-29 08:30:20'),
-('f2def977806d6854648a6ddcaab81d343dd3afa4', 'bike2', '56ed9224022fae5119c9bf9be48d2f4cc0d4dc85', '2016-12-29 08:21:38', '2016-12-29 08:21:38'),
-('fb66445ec487a9013f2329dcbff239083556a0cf', 'Bike', '56ed9224022fae5119c9bf9be48d2f4cc0d4dc85', '2016-12-29 08:21:38', '2016-12-29 08:21:38');
+('2', '2', '3', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+--
+-- Dumping data for table `skill`
+--
+
+INSERT INTO `skill` (`skill_id`, `skill`, `createdAt`, `updatedAt`) VALUES
+('1', 'Bike', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('2', 'handsome', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+--
+-- Dumping data for table `toolship`
+--
+
+INSERT INTO `toolship` (`toolship_id`, `user_id`, `mission_id`, `rating`, `feedback`, `createdAt`, `updatedAt`) VALUES
+('1', '7a14025d87f837aedb17c502b3231b49', '1', NULL, NULL, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('2', '6399cc0b0a477dc1e4d9bcc43f652e29', '1', NULL, NULL, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('3', '19dddecfa2176953057f16f63ba3d52f', '2', NULL, NULL, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('4', '6399cc0b0a477dc1e4d9bcc43f652e29', '3', NULL, NULL, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `account`, `password`, `gender`, `photo_url`, `user_name`, `email`, `createdAt`, `updatedAt`) VALUES
+('19dddecfa2176953057f16f63ba3d52f', 'wei', 'wei', 'B', '/assets/images/tool.png', 'wei', 'wei', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('6399cc0b0a477dc1e4d9bcc43f652e29', 'pu', 'pu', 'B', '/assets/images/tool.png', 'pu', 'pu', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('7a14025d87f837aedb17c502b3231b49', 'rick', 'rick', 'B', '/assets/images/tool.png', 'rick', 'rick', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('cdf02830e6eba153d68595d64f5d397a', 'E', 'E', 'G', '/assets/images/ee.jpg', 'EE', 'EE', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+('e59bf7cd0b41e35b7ba50fc8e71cdf64', 'J', 'J', 'B', '/assets/images/tool.png', 'J', 'J', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+--
+-- Dumping data for table `user_skill`
+--
+
+INSERT INTO `user_skill` (`user_skill_id`, `skill_id`, `user_id`, `createdAt`, `updatedAt`) VALUES
+('1', '1', '19dddecfa2176953057f16f63ba3d52f', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
